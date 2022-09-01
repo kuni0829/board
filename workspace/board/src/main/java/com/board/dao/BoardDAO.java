2b2fa285de16275f67f7a0ccba62e84e2d1d7058
kuni <kuni@leeni.co.kr>
2022-09-01 11:11:58 +0900

    board 프로젝트 파일

---
+++ workspace/board/src/main/java/com/board/dao/BoardDAO.java
@@ -0,0 +1,17 @@
+package com.board.dao;
+
+import java.util.List;
+
+import com.board.domain.BoardVO;
+
+public interface BoardDAO {
+ 
+	// 게시판 목록
+	public List<BoardVO> list() throws Exception; 
+
+	// 게시판 작성
+	public void write(BoardVO vo) throws Exception;
+
+	// 게시판 조회
+	public void view(BoardVO vo) throws Exception;
+}
